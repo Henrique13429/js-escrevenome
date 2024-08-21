082c3eb1534e19629d404e01d3848851910197f6
Henrique <00001099944120sp@al.educacao.sp.gov.br>
2024-08-21 17:30:23 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+    background("black")
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
